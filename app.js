802dc7db20e389fca2748a7c035e864ecfa75668
Ntd1611 <trungduc3326@gmail.com>
2020-06-28 00:00:25 -0400

    second commit. Add error handler

--- app.js
+++ app.js
@@ -19,9 +19,6 @@ function calculateResult(e) {
   const calculatedInterest = parseFloat(interest.value) / 100 / 12;
   const calculatedPayments = parseFloat(years.value) * 12;
 
-  console.log(principal);
-  console.log(calculatedInterest);
-  console.log(calculatedPayments);
   // Compute monthly payment
   const x = Math.pow(1 + calculatedInterest, calculatedPayments);
   const monthly = (principal * x * calculatedInterest) / (x - 1);
@@ -31,6 +28,26 @@ function calculateResult(e) {
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
   } else {
-    console.log("Check your number");
+    showError("Please check your number");
+  }
+
+  function showError(error) {
+    // Create a div
+    const errorDiv = document.createElement("div");
+    // Get elements
+    const card = document.querySelector(".card");
+    const heading = document.querySelector(".heading");
+    // Add class
+    errorDiv.className = "alert alert-danger";
+    // Create text note and append to div
+    errorDiv.appendChild(document.createTextNode(error));
+    // Insert error above heading
+    card.insertBefore(errorDiv, heading);
+    // Clear error after 2s
+    setTimeout(clearError, 2000);
+  }
+
+  function clearError() {
+    document.querySelector(".alert").remove();
   }
 }
